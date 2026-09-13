763387d17faf1294fb62640cabd462c37a95430f
Mohammad Salim <129617414+mohammad-salim-23@users.noreply.github.com>
2026-09-13 18:25:07 +0600

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int>s;
+        vector<int>ans;
+        int n = temperatures.size();
+        for(int i = n-1;i>=0;i--){
+            if(s.empty()){
+                s.push(i);
+                ans.push_back(0);
+            }else{
+                while(!s.empty() && temperatures[s.top()]<=temperatures[i]){
+                    s.pop();
+                }
+                if(s.empty()){
+                    s.push(i);
+                    ans.push_back(0);
+                }else{
+                    ans.push_back(s.top()-i);
+                    s.push(i);
+                }
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
